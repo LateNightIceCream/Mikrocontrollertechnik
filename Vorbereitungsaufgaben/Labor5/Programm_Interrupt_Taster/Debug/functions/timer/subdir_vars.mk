c50596f63840e6d86d9ec1faea91ea899b36a794
LateNightIceCream <richardgruenert@live.com>
2020-04-16 10:16:05 +0200

    add program

---
+++ Vorbereitungsaufgaben/Labor5/Programm_Interrupt_Taster/Debug/functions/timer/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../functions/timer/timer_A.c 
+
+C_DEPS += \
+./functions/timer/timer_A.d 
+
+OBJS += \
+./functions/timer/timer_A.obj 
+
+OBJS__QUOTED += \
+"functions/timer/timer_A.obj" 
+
+C_DEPS__QUOTED += \
+"functions/timer/timer_A.d" 
+
+C_SRCS__QUOTED += \
+"../functions/timer/timer_A.c" 
+
+
